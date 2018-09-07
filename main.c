026a269db09e131dd7b7ef8d82d0b35edb0cca7b
Johan Brun <johannicbrun@gmail.com>
2018-09-07 19:35:34 +0200

    I don't remeber

--- main.c
+++ main.c
@@ -21,18 +21,27 @@ int main()
     USART_Init (MYBURR);
     // led_init();
     MCUCR |= (1 << SRE);
+    //SFIOR |= (1 << XMM2);
     fdevopen(*USART_Transmit, *USART_Receive);
+    volatile char *ext_ram = (char *) 0xFFFF;
     double T = 1000.00;
-    volatile char *ext_ram = (char *) 0x1800;
+    uint8_t retreived_value;
+    uint8_t some_value = 0xFF;
+    uint16_t i = 11;
 
     while(1)
     {
-        ext_ram[10] = 'a';
         //SRAM_test();
+
+        ext_ram[0] = some_value;
+        retreived_value = ext_ram[0];
+        if (retreived_value != some_value) {
+            printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n\r", i, retreived_value, some_value);
+        }
         //USART_Transmit('z');
         _delay_ms(T);
 	    //printf("Test it");
-        printf("Transmitting data to the RAM  ");
+        printf("Transmitting data to the RAM\n\r");
     }
 }
 
@@ -43,7 +52,7 @@ void SRAM_test(void)
     uint16_t ext_ram_size = 0x800;
     uint16_t write_errors = 0;
     uint16_t retrieval_errors = 0;
-    printf("Starting SRAM test...\n");
+    printf("Starting SRAM test...\n\r");
     // rand() stores some internal state, so calling this function in a loop will
     // yield different seeds each time (unless srand() is called before this function)
     uint16_t seed = rand();
@@ -52,9 +61,10 @@ void SRAM_test(void)
     for (uint16_t i = 0; i < ext_ram_size; i++) {
         uint8_t some_value = rand();
         ext_ram[i] = some_value;
+        _delay_ms(1000.00);
         uint8_t retreived_value = ext_ram[i];
         if (retreived_value != some_value) {
-            printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i, retreived_value, some_value);
+            printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n\r", i, retreived_value, some_value);
             write_errors++;
         }
 }
