b0097b7a7405b5d31c4490487d3d1492ad0e3915
Giovanni Zenari <giovannizenari@Giovannis-MacBook-Pro.local>
2018-10-10 20:55:11 +0200

    Menu improvement 1

--- SPI.c
+++ SPI.c
@@ -4,24 +4,28 @@
 
 void SPI_MasterInit()
 {
-  /* Set MOSI and SCK output, all others input */
+  /* Set MOSI, SCK and !SS output, all others input */
   DDRB = (1<<DDB5)|(1<<DDB7)|(1<<DDB4);
 
   /* Enable SPI, Master, set clock rate fck/16 */
   SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+    
+  /* Set SS high, deselect CAN-controller */
   PORTB |= (1<<PB4);
 }
 
 void SPI_MasterTransmit(char cData)
 {
-  //PORTB &= ~ (1<< PB4);
   /* Start transmission */
   SPDR = cData;
   /* Wait for transmission complete */
   while(!(SPSR & (1<<SPIF)));
 
+  // When we finish to transmit the SPIF register goes to 1 and we need to reset it
+  // if we want to transmit again.This is done
+  // first by reading the SPSR register with SPIF set, loop above, and then accesing the SPI data
+  // data register, this line.
   cData = SPDR;
-  //PORTB |= (1<<PB4);
 }
 
 void SPI_SlaveInit()
--- menu_interface.c
+++ menu_interface.c
@@ -1,20 +1,9 @@
-#include <stdio.h>
-#include <avr/io.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <avr/pgmspace.h>
-
-#define F_CPU 4915200UL
-#include <util/delay.h>
-
-#include "fonts.h"
-
-#include "joystick.h"
-#include "oled_interface.h"
-
-volatile char *OLED_cmd = (char *) 0x1000;
-volatile char *OLED_data = (char *) 0x1200;
+// The main purpouse of this interface is to
+// allow the user to manage the menu and leave all the settings
+// to the oled_interface. The main point to remember here is that
+// is responsibility of the caller to set the starting position with the
+// corresponding function for coloumn and page and this must be done everytime
+// you want to print something.
 
 bool treshold_UP = true;
 bool treshold_DOWN = true;
@@ -29,8 +18,25 @@ typedef struct menu_item {
 typedef struct node {
     menu_item* menu;
     struct node * next;
-    // Add another node for the submenu
+    // Submenu
+    struct node * down;
 } node;
+
+int level = 0;
+
+void initialiseNode(node* new_node, char* title, int selection, int length, int menu_position)
+{
+  new_node->menu = (struct menu_item*)malloc(sizeof(struct menu_item));
+
+  new_node->menu->selection = 1;
+  new_node->menu->menu_position = 0;
+  new_node->menu->length = 4;
+
+  new_node->menu->title = malloc(strlen(name)+1);
+  strcpy(new_node->menu->title, name);
+
+  new_node->next = NULL;
+}
 
 void push(node * head, char* title, int selection, int length, int menu_position)
 {
@@ -41,6 +47,9 @@ void push(node * head, char* title, int selection, int length, int menu_position
     }
 
     current->next = (struct node*)malloc(sizeof(struct node));
+
+    initialiseNode(current->next, title, selection, length, menu_position);
+
     current->next->menu = (struct menu_item*)malloc(sizeof(struct menu_item));
 
     current->next->menu->selection = selection;
@@ -48,7 +57,7 @@ void push(node * head, char* title, int selection, int length, int menu_position
     current->next->menu->length = length;
 
     current->next->menu->title = malloc(strlen(title)+1);
-    strcpy(current->next->menu->title,title);
+    strcpy(current->next->menu->title, title);
 
     current->next->next = NULL;
 }
@@ -57,6 +66,9 @@ node* Menu_Init(){
     char name[] = {'M','E','N','U'};
 
     struct node* new_node = (struct node*)malloc(sizeof(struct node));
+
+    initialiseNode(new_node, "MENU", selection, length, menu_position);
+
     new_node->menu = (struct menu_item*)malloc(sizeof(struct menu_item));
 
     new_node->menu->selection = 1;
@@ -64,7 +76,7 @@ node* Menu_Init(){
     new_node->menu->length = 4;
 
     new_node->menu->title = malloc(strlen(name)+1);
-    strcpy(new_node->menu->title,name);
+    strcpy(new_node->menu->title, name);
 
     new_node->next = NULL;
 
@@ -72,143 +84,6 @@ node* Menu_Init(){
     push(new_node, "EXTRAS", 0, 6, 1);
 
     return new_node;
-}
-
-void OLED_Init()
-{
-    //Setup the OLED display
-
-	//display off
-	*OLED_cmd = 0xAE;
-	//segment remap
-	*OLED_cmd = 0xA1;
-
-	//common pads hardware: alternative
-	*OLED_cmd = 0xDA;
-	*OLED_cmd = 0x12;
-
-	//common output scan direction:com63~com0
-	*OLED_cmd = 0xC8;
-
-	//multiplex ration mode:63
-	*OLED_cmd = 0xA8;
-	*OLED_cmd = 0x3F;
-
-	//display divide ratio/osc. freq. mode
-	*OLED_cmd = 0xD5;
-	*OLED_cmd = 0x80;
-
-	//contrast control
-	*OLED_cmd = 0x81;
-	*OLED_cmd = 0x50;
-
-	//set pre-charge period
-	*OLED_cmd = 0xD9;
-	*OLED_cmd = 0x21;
-
-	//set Memory Addressing Mode
-	*OLED_cmd = 0x20;
-	*OLED_cmd = 0x00;
-
-	//VCOM deselect level mode
-	*OLED_cmd = 0xDB;
-	*OLED_cmd = 0x30;
-
-	//master configuration
-	*OLED_cmd = 0xAD;
-	*OLED_cmd = 0x00;
-
-	//out follows RAM content
-	*OLED_cmd = 0xA4;
-
-	//set normal display
-	*OLED_cmd = 0xA6;
-	//display on
-	*OLED_cmd = 0xAF;
-
-    //Setup colum start and end adress
-	*OLED_cmd = 0x21;
-	//Column start adress
-	*OLED_cmd = 0x00;
-	//Column end adress
-	*OLED_cmd = 0x7F;
-
-    //Set up page start and end adress
-    *OLED_cmd = 0x22;
-    //Page start adress
-	*OLED_cmd = 0x00;
-	//Page end adress
-	*OLED_cmd = 0x07;
-}
-
-void oledSendData(char c)
-{
-  *OLED_data = c
-}
-
-void oledPrintChar(char c)
-{
-  int character = c - 32;
-  OLED_data[0] = pgm_read_byte(&(font8[character][0]));
-  OLED_data[0] = pgm_read_byte(&(font8[character][1]));
-  OLED_data[0] = pgm_read_byte(&(font8[character][2]));
-  OLED_data[0] = pgm_read_byte(&(font8[character][3]));
-  OLED_data[0] = pgm_read_byte(&(font8[character][4]));
-  OLED_data[0] = pgm_read_byte(&(font8[character][5]));
-  OLED_data[0] = pgm_read_byte(&(font8[character][6]));
-  OLED_data[0] = pgm_read_byte(&(font8[character][7]));
-}
-
-void OLED_setup_column_adress(char* start_adress, char* end_adress)
-{
-    //Setup colum start and end adress
-	*OLED_cmd = 0x21;
-	//Column start adress
-	*OLED_cmd = start_adress;
-	//Column end adress
-	*OLED_cmd = end_adress;
-}
-
-void OLED_setup_page_adress(char* start_adress, char* end_adress)
-{
-    //Setup colum start and end adress
-	*OLED_cmd = 0x22;
-	//Column start adress
-	*OLED_cmd = start_adress;
-	//Column end adress
-	*OLED_cmd = end_adress;
-}
-
-void oled_print(char* string, int length)
-{
-    for (size_t i = 0; i < length; i++) {
-        int character = string[i] - 32;
-        OLED_data[0] = pgm_read_byte(&(font8[character][0]));
-        OLED_data[0] = pgm_read_byte(&(font8[character][1]));
-        OLED_data[0] = pgm_read_byte(&(font8[character][2]));
-        OLED_data[0] = pgm_read_byte(&(font8[character][3]));
-        OLED_data[0] = pgm_read_byte(&(font8[character][4]));
-        OLED_data[0] = pgm_read_byte(&(font8[character][5]));
-        OLED_data[0] = pgm_read_byte(&(font8[character][6]));
-        OLED_data[0] = pgm_read_byte(&(font8[character][7]));
-    }
-}
-
-void oled_clear()
-{
-    OLED_setup_column_adress(0x00, 0x7F);
-    OLED_setup_page_adress(0x00, 0x07);
-    for (size_t i = 0; i < 8; i++) {
-        for (size_t i = 0; i < 128; i++) {
-            *OLED_data = 0x00;
-        }
-    }
-}
-
-void oled_reset()
-{
-    OLED_setup_column_adress(0x00, 0x7F);
-    OLED_setup_page_adress(0x00, 0x07);
 }
 
 void Menu_Print(void* menu)
@@ -296,7 +171,7 @@ void clear_selection()
     OLED_setup_column_adress(0x00, 0x0A);
     OLED_setup_page_adress(0x00, 0x7F);
     for (size_t i = 0; i < 300; i++) {
-        *OLED_data = 0x00;
+        oledSendData(0x00)
     }
 }
 
@@ -305,7 +180,7 @@ void clear_menu()
     OLED_setup_column_adress(0x0B, 0x0B);
     OLED_setup_page_adress(0x00, 0x7F);
     for (size_t i = 0; i < 3000; i++) {
-        *OLED_data = 0x00;
+        oledSendData(0x00);
     }
 }
 
